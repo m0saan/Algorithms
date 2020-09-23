d3467d162a900168ea1626b2defe7aabadaa82fe
MoBoustta <moboustta6@gmail.com>
2020-09-23 23:40:51 +0100

    Algorithms<Bubble Sort>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <vector>
+#include "bubble_sort.cpp"
+
+std::ostream& operator<<(std::ostream& o, std::vector<int>& vector){
+    for(auto & elem : vector)
+        o << elem << ' ';
+    return o;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    std::vector<int> v { 8, 2, 4, 1, 3 };
+    bubbleSort(v);
+    std::cout << v << std::endl;
     return 0;
 }
