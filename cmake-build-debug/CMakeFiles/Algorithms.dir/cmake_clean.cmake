cbc3cb180a4aac029f0fdd072b3d6fd0ac058d07
MoBoustta <moboustta6@gmail.com>
2020-09-23 23:09:42 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/Algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Algorithms.exe"
+  "Algorithms.exe.manifest"
+  "Algorithms.pdb"
+  "CMakeFiles/Algorithms.dir/main.cpp.obj"
+  "libAlgorithms.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
