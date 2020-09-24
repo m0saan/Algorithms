8c536519e78e69cb1a61a85b632ec2ec5ff3f889
MoBoustta <moboustta6@gmail.com>
2020-09-24 12:11:18 +0100

    Algorithms<SelectionSort and BubbleSort Tests>

--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -31,3 +31,27 @@ TEST(BubbleSort, EmptyArray){
     ASSERT_EQ(v, v2);
 
 }
+
+TEST(SelectionSort, OddNumOfElementsCase) {
+
+    std::vector<int> v{8, 2, 4, 1, 3};
+    std::vector<int> vResult{1, 2, 3, 4, 8};
+
+    selectionSort(v);
+    ASSERT_EQ(v, vResult);
+}
+
+TEST(SelectionSort, EvenNumOfElementsCase){
+    std::vector<int> v{8, 2};
+    selectionSort(v);
+    ASSERT_EQ(v, (std::vector<int> { 2, 8 }));
+}
+
+TEST(SelectionSort, EmptyArray){
+    std::vector<int> v{};
+    auto v2 = v;
+    bubbleSort(v);
+
+    ASSERT_EQ(v, v2);
+
+}
