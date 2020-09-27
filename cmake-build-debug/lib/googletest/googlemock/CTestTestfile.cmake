eb11153e84eaa62b2fc4b73b1fe83f589c4aec93
MoBoustta <moboustta6@gmail.com>
2020-09-28 00:12:27 +0100

    Remove unused folders and files

--- cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/one for all/CLionProjects/Algorithms/lib/googletest/googlemock
-# Build directory: C:/Users/one for all/CLionProjects/Algorithms/cmake-build-debug/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
