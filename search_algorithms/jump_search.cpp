15d77ee8c5bb2d56512a77ea580fd2cf965f4be5
MoBoustta <moboustta6@gmail.com>
2020-09-28 19:26:47 +0100

    Implement Jump Search

---
+++ search_algorithms/jump_search.cpp
@@ -0,0 +1,21 @@
+//
+// Created by moboustt on 9/28/20.
+//
+
+#include <vector>
+#include <cmath>
+
+int jumpSearch(std::vector<int>& vector, const int& target) {
+    size_t blockSize = sqrt(vector.size());
+    size_t start = 0;
+    size_t next = blockSize;
+    while (start < vector.size() && next <= vector.size()){
+        if (target <= vector[start + blockSize]){
+            while (start < start + blockSize)
+                if (vector[start++] == target) return static_cast<int>(start -1);
+        }
+        start = next;
+        next += blockSize;
+    }
+    return -1;
+}
